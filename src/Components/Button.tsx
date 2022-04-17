94ff2abd8bf9a7a7b1574f2d43b6f22b3966a28b
Russell Chien <RussellChien6@gmail.com>
2022-04-17 12:08:07 -0700

    initial commit

---
+++ src/Components/Button.tsx
@@ -0,0 +1,13 @@
+export interface ButtonProps {
+  click: Function;
+  buttonText: string;
+}
+export const Button = ({ click, buttonText }: ButtonProps) => {
+  return (
+    
+    <button className="rounded-md bg-purple-700 px-4" onClick={() => click()}>
+      {buttonText}
+    </button>
+    
+  );
+};
